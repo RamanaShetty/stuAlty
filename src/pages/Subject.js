ef068a6fb95f99d0bd4e6e7efaa3941c14bd0a8c
Ramana Gowrishetty <ramana.03shetty@gmail.com>
2023-11-20 12:58:08 +0530

    renamed

--- src/pages/Subject.js
+++ src/pages/Subject.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { db } from "../configuratoin/firebaseConfig";
+import { db } from "../configuration/firebaseConfig";
 import {
   getDoc,
   doc,
